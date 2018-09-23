186119d7bc21d1910b3b93bc8414c794682ecb47
Masud Morshed <masudraj6@gmail.com>
2018-09-24 05:17:57 +0600

    article template created

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("author/<name>", views.getauthor, name="author")
+    path('author/<name>', views.getauthor, name="author"),
+    path('article/<int:id>', views.getsingle, name='single_post')
 ]
